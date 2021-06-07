006c246b525bb408a777bf68ec9067cad2c145c1
HoussemRabah <63176254+HoussemRabah@users.noreply.github.com>
2021-06-07 15:47:02 +0100

    07/06/2021

--- orientation.py
+++ orientation.py
@@ -1,48 +1,40 @@
-import converter as cv
+import pandas as pn
+
 
 specialite = {
     "il": 1,
-    "rsd": 2,
+    "rsd": 2, 
     "sii": 2,
     "ssi": 1,
     "mind": 1,
-    "iv": 2,
+    "iv": 2, 
     "bio-info": 1
 }
 
 
 
+
+# Vérifier si on peut donner le choix à l'étudiant
 def isAvailble(choix, specialite):
     if(specialite[choix] > 0):
         return True
     else:
         return False
 
-
-def getChoixOf(etudiant, specialite):
-    for i in range(1, 5):
-        if(isAvailble(etudiant["choix{}".format(i)], specialite)):
-            return etudiant["choix{}".format(i)]
+# Obtention du choix final de l'étudiant
+def getChoixOf(lesChoix, specialite):
+    for choix in lesChoix:
+        if(isAvailble(choix, specialite)):
+            return choix
     return "empty"
 
 
-def affecter(etudiant, specialite):
-    choix = getChoixOf(etudiant, specialite)
+# Renvoie un dictionnaire contenant matricule de l'étudiant et la spécialisation à laquelle il a été envoyé
+def affecter(ficheVoeux, specialite):
+    choix = getChoixOf(ficheVoeux["lesChoix"], specialite)
     etudiantData = {
-        "MAT": etudiant["MAT"],
-        "NAME": etudiant["NAME"],
-        "PNAME": etudiant["PNAME"],
+        "MAT": ficheVoeux["matricule"],
         "choix": choix
     }
     return etudiantData
-
-def affecterTous(listEtudiants, specialite):
-    listEtudiantsFinal = []
-    for etudiant in listEtudiants:
-       listEtudiantsFinal.append(affecter(etudiant, specialite))
-       choix = getChoixOf(etudiant, specialite)
-       if(choix != "empty"):
-         specialite[choix] = specialite[choix]-1
-    return listEtudiantsFinal 
-
 
