bad6e25cc16c94bfed75ca5faa68d0e9891ac657
HoussemRabah <63176254+HoussemRabah@users.noreply.github.com>
2021-06-14 16:57:00 +0100

    14/06/2021 (functions)

--- input.py
+++ input.py
@@ -1,30 +1,44 @@
+from os import remove
 import converter as cn
+import pandas as pa
+import numpy as nu
 
 
-
+# load config file
+def loadConfig(filename):
+    f = open(filename, "r")
+    return f.read()
 
 # input the excel file of "classement" as a list of dictionary 'classement must be sorted'
-def getClassement(filename, sheetname):
+def loadClassement(filename, sheetname):
     cn.excelToCSV(filename, sheetname, "\\temp.csv")
     return cn.CSVToDict("\\temp.csv")
 
+
 # input excel files of "les fiche de voeux" as a list of dictionary
 # listFilename : list of filenames of "les fiches" (fiche de isil,fiche de acad , fiche de gtr)
-def getFicheVoeux(listFilename, listSheetname):
+def loadFicheVoeux(listFilename, listSheetname):
     dic=[]
     i=0
     for filename in listFilename:
         cn.excelToCSV(filename, listSheetname[i] , "\\temp.csv")
-        dic.append(cn.CSVToDict("\\temp.csv"))
+        dic=dic+(cn.CSVToDict("\\temp.csv"))
         i=i+1
     return dic #format : {"maticule": xxxx, "il":1, "vi":3, "mind":2}
 
 
+# step 2 : 
 
 #  {"maticule": xxxx, "il":1, "vi":3, "mind":2} ==> ("il", "mind", "vi") "with startIndex = 1"
 def createListOfChoix(ficheVoeux, startIndex):
     choixOrdre = list(ficheVoeux.values())[startIndex:]        # example : (1,3,4,2)
     choixColumns = list(ficheVoeux.keys())[startIndex:]        # example : (il,vi,bio,si)
+    # remplir les choix vides avec des values increment
+    i=0
+    for choi in choixOrdre:
+        if(pa.isna(choi)):
+            choixOrdre[i]= nu.nanmax(choixOrdre)+1
+        i=i+1
     return list(zip(*sorted(zip(choixOrdre,choixColumns))))[1] # alors   : (il,si,vi,bio)
     
 
@@ -48,5 +62,23 @@ def sortListOfFicheVoeux(listFicheVoeux , listEtudiantsSorted) :
     e=[]  
     for etd in listEtudiantsSorted :
         e.append(etd['MAT'])
-    listFicheVoeux.sort(key = lambda i: e.index(i['matricule']))
-    return listFicheVoeux
+    
+    # Deleting a student from classement if he does not have a fiche de voeux (Did not choose a master)
+    for mat in e:
+        if not (mat in [x['matricule'] for x in listFicheVoeux]):
+            e= [i for i in e if i != mat]
+
+
+    # Deleting a student fiche de voeux if he does not exist in classement (L3 n'est pas valider)
+    for fiche in listFicheVoeux:
+        if not (fiche["matricule"] in e):
+            listFicheVoeux= [i for i in listFicheVoeux if i["matricule"] != fiche["matricule"]]
+
+
+    try:
+        listFicheVoeux.sort(key = lambda i: e.index(i['matricule']))
+        return listFicheVoeux
+    except ValueError as e:
+        print(e)
+        return []
+    
