5ae4c2184b7f235a9c961019d903a31c039b024a
HoussemRabah <63176254+HoussemRabah@users.noreply.github.com>
2021-06-21 18:02:42 +0100

    21/06/2021 (functions)

--- calculClassement.py
+++ calculClassement.py
@@ -1,20 +1,17 @@
 
-import parser as pr
-
 
 def calculerMoy(listEtudiants, formulaire):     # ajouter column de moyen dont list etudiant
     for etudiant in listEtudiants:
-       etudiant["MC"]=parseFormulaire(formulaire, etudiant)
-     
+       etudiant["MC"]=parseFormule(formulaire, etudiant)
+    
     return listEtudiants
 
 def sortClassement(listEtudiants):
     return sorted(listEtudiants, key=lambda k: k["MC"], reverse=True) 
 
 
-def parseFormulaire(formulaire , dict):
-    for i in range(1,len(formulaire.keys())):
-        formulaire["form"]=formulaire["form"].replace(("$"+str(i)+"$"),"dict[\"" + formulaire[str(i)]+"\"]")
+def parseFormule(formule , dict):
+    for i in range(1,len(formule.keys())):
+        formule["form"]=formule["form"].replace(("$"+str(i)+"$"),"dict[\"" + formule[str(i)]+"\"]")
     
-    return eval(formulaire["form"])
- 
+    return eval(formule["form"])
