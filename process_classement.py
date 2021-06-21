7dd1d4ed7d8aba4852ca0ec57f2124ffcec78d9e
HoussemRabah <63176254+HoussemRabah@users.noreply.github.com>
2021-06-21 15:24:17 +0100

    21/06/2021

---
+++ process_classement.py
@@ -0,0 +1,20 @@
+import calculClassement as cc
+import converter as cn
+from input import loadConfig , loadFile
+import json
+
+
+
+# load config 
+jsontxt= loadConfig("config.json")
+config = json.loads(jsontxt)
+
+# load list etudiant
+listEtudiant = loadFile(config["listEtudiants"]["filename"], config["listEtudiants"]["sheetname"])
+
+# preparer classement
+listEtudiant = cc.calculerMoy(listEtudiant, config["formulaire"])
+listEtudiant = cc.sortClassement(listEtudiant)
+
+# output classement
+cn.dictToExcel(listEtudiant, config["output"]["classement"])
