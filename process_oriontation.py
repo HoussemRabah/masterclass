e114abd9ec3654b73fc7c1829f4fc85b1d562b23
HoussemRabah <63176254+HoussemRabah@users.noreply.github.com>
2021-07-01 18:14:44 +0100

    01/07/2021

--- process_oriontation.py
+++ process_oriontation.py
@@ -4,40 +4,45 @@ from input import createListOfChoix, loadFile, loadConfig , loadFicheVoeux , cre
 import json
 import converter as cn
 
+def run ():
+    print("Charger les confugirations à partir de config.json ...")
 
 # step 1 : load config file and excel files (classement must be sorted)
-jsontxt= loadConfig("config.json")
-config = json.loads(jsontxt)
-classement= loadFile(config["classement"]["filename"], config["classement"]["sheetname"])
-listFilesOfFDV = [i["filename"] for i in config["ficheVoeux"]]
-listSheetsOfFDV = [i["sheetname"] for i in config["ficheVoeux"]]
-listFicheVoeux= loadFicheVoeux(listFilesOfFDV, listSheetsOfFDV)
+    jsontxt= loadConfig("config.json")
+    config = json.loads(jsontxt)
+    classement= loadFile(config["classement"]["filename"], config["classement"]["sheetname"])
+    listFilesOfFDV = [i["filename"] for i in config["ficheVoeux"]]
+    listSheetsOfFDV = [i["sheetname"] for i in config["ficheVoeux"]]
+    listFicheVoeux= loadFicheVoeux(listFilesOfFDV, listSheetsOfFDV)
     
 
-
+    print("Traitement des données ...")
 # step 2 : Prepare dictionaries
-listFicheVoeux = createListOfFicheVoeux(listFicheVoeux, config["choixStartIndex"])
-listFicheVoeux = sortListOfFicheVoeux(listFicheVoeux, classement)
-specialite = config["specialites"]
+    listFicheVoeux = createListOfFicheVoeux(listFicheVoeux, config["choixStartIndex"])
+    listFicheVoeux = sortListOfFicheVoeux(listFicheVoeux, classement)
+    specialite = config["specialites"]
 
 # step 3 : Check that everything is good
-everythingGood = True
-totalPlace=0
-for s in specialite.keys():
-    totalPlace=totalPlace+specialite[s]
-if(not len(listFicheVoeux)<=totalPlace):
-    everythingGood=False
-    print("Les places en master ne suffisent pas à tous les étudiants")
+    everythingGood = True
+    totalPlace=0
+    for s in specialite.keys():
+      totalPlace=totalPlace+specialite[s]
+    if(not len(listFicheVoeux)<=totalPlace):
+        everythingGood=False
+        print("Les places en master ne suffisent pas à tous les étudiants")
 
 
-# step 4 : fait oriontation
-if(everythingGood):
-    classementFinal=[]
-    for ficheVoeux in listFicheVoeux:
-        classementFinal.append(affecter(ficheVoeux, specialite))
-        choix = getChoixOf(ficheVoeux["lesChoix"], specialite)
-        specialite[choix]=specialite[choix]-1
+    # step 4 : fait oriontation
+    if(everythingGood):
+        classementFinal=[]
+        for ficheVoeux in listFicheVoeux:
+            classementFinal.append(affecter(ficheVoeux, specialite))
+            choix = getChoixOf(ficheVoeux["lesChoix"], specialite)
+            specialite[choix]=specialite[choix]-1
     
+        print("Sauvegarder ...")
     # step 5 : output
-    classementFinal=mergeTwoDict(classementFinal, classement, "MAT")
-    cn.dictToExcel(classementFinal, config["output"]["orientation"])
+        classementFinal=mergeTwoDict(classement,classementFinal, "MAT")
+        cn.dictToExcel( classementFinal, config["output"]["orientation"])
+        print("Terminé avec succès en :")
+        print(config["output"]["orientation"])
