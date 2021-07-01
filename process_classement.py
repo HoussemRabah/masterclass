7aa8b86802ea7efc6574c9288ade3779e49d6ac4
HoussemRabah <63176254+HoussemRabah@users.noreply.github.com>
2021-07-01 18:12:46 +0100

    01/07/2021

--- process_classement.py
+++ process_classement.py
@@ -4,17 +4,24 @@ from input import loadConfig , loadFile
 import json
 
 
-
+def run() :
+    print("Charger les confugirations à partir de config.json ...")
 # load config 
-jsontxt= loadConfig("config.json")
-config = json.loads(jsontxt)
+    jsontxt= loadConfig("config.json")
+    config = json.loads(jsontxt)
 
 # load list etudiant
-listEtudiant = loadFile(config["listEtudiants"]["filename"], config["listEtudiants"]["sheetname"])
+    listEtudiant = loadFile(config["listEtudiants"]["filename"], config["listEtudiants"]["sheetname"])
 
+    print("Traitement des données ...")
 # preparer classement
-listEtudiant = cc.calculerMoy(listEtudiant, config["formule"])
-listEtudiant = cc.sortClassement(listEtudiant)
+    listEtudiant = cc.calculerMoy(listEtudiant, config["formule"])
+    listEtudiant = cc.sortClassement(listEtudiant)
 
+    print("Sauvegarder ...")
 # output classement
-cn.dictToExcel(listEtudiant, config["output"]["classement"])
+    cn.dictToExcel(listEtudiant, config["output"]["classement"])
+
+    
+    print("Terminé avec succès en :")
+    print(config["output"]["classement"])
